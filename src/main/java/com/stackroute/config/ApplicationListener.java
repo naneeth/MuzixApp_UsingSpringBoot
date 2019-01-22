f0150148d99f8ae2cdbade9beb204de9ff9b99a7
naneeth <naneethc003@gmail.com>
2019-01-22 23:22:11 +0530

    INITIAL

---
+++ src/main/java/com/stackroute/config/ApplicationListener.java
@@ -0,0 +1,33 @@
+package com.stackroute.config;
+
+import com.stackroute.domain.Track;
+import com.stackroute.service.TrackService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationListener implements org.springframework.context.ApplicationListener<ContextRefreshedEvent> {
+
+    TrackService trackService;
+
+    @Autowired
+    public ApplicationListener(TrackService trackService) {
+        this.trackService = trackService;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+
+        Track track = new Track();
+        track.setTrackId(0);
+        track.setTrackName("default");
+        track.setComments("default comments");
+        try {
+            trackService.saveTrack(track);
+        }
+        catch (Exception e){}
+    }
+
+
+}
